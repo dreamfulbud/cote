5411137f461ec9ce2796d7cd01c8cdb56ff0be55
dreamfulbud <dreamfulbud@naver.com>
2021-11-26 13:25:49 +0900

    2주차 코테

--- 2week/001.js
+++ 2week/001.js
@@ -25,12 +25,13 @@ function solution(new_id) {
   }
   // 6단계 new_id의 길이가 16자 이상이면, new_id의 첫 15개의 문자를 제외한 나머지 문자들을 모두 제거합니다.
   //      만약 제거 후 마침표(.)가 new_id의 끝에 위치한다면 끝에 위치한 마침표(.) 문자를 제거합니다.
-  if (answer.length > 15) {
+  if (new_id.length > 15) {
     new_id = new_id.substr(0, 15);
   }
   if (new_id[new_id.length - 1] === '.') {
     new_id = new_id.slice(0, -1);
   };
+
 
   // 7단계 new_id의 길이가 2자 이하라면, new_id의 마지막 문자를 new_id의 길이가 3이 될 때까지 반복해서 끝에 붙입니다.
   let len = new_id.length;
--- 2week/002.js
+++ 2week/002.js
@@ -4,28 +4,61 @@
 function solution(numbers, hand) {
   var answer = '';
 
-  let leftPos = '';
-  let rightPos = '';
+  let leftPos = 10;
+  let rightPos = 12;
 
-  for (let i = 0; i < numbers.length; i++) {
-    if (numbers[i] === 1 || numbers[i] === 4 || numbers[i] === 7) {
-      leftPos = numbers[i];
-      numbers[i] = "L";
-    } else if (numbers[i] === 3 || numbers[i] === 6 || numbers[i] === 9) {
-      rightPos = numbers[i];
-      numbers[i] = "R";
-    } else if (numbers[i] === 2) {
-
+  for (let n = 0; n < numbers.length; n++) {
+    if (numbers[n] === 0) {
+      numbers[n] = 11;
     }
+    if (numbers[n] === 1 || numbers[n] === 4 || numbers[n] === 7) {
+      leftPos = numbers[n];
+      numbers[n] = "L";
 
+    } else if (numbers[n] === 3 || numbers[n] === 6 || numbers[n] === 9) {
+      rightPos = numbers[n];
+      numbers[n] = "R";
+    } else {
+      let l = Math.abs(numbers[n] - leftPos);
+      let r = Math.abs(numbers[n] - rightPos);
+
+      if (l === 1 || l === 3) {
+        l = 1;
+      } else if (l === 2 || l === 4) {
+        l = 2;
+      } else if (l === 5 || l === 6 || l === 7) {
+        l = 3;
+      }
+
+      if (r === 1 || r === 3) {
+        r = 1;
+      } else if (r === 2 || r === 4) {
+        r = 2;
+      } else if (r === 5 || r === 6 || r === 7) {
+        r = 3;
+      }
+
+      if (l === r) {
+        if (hand === "right") {
+          rightPos = numbers[n];
+          numbers[n] = "R";
+        } else {
+          leftPos = numbers[n];
+          numbers[n] = "L";
+        }
+      } else if (l > r) {
+        rightPos = numbers[n];
+        numbers[n] = "R";
+      } else {
+        leftPos = numbers[n];
+        numbers[n] = "L";
+      }
+    }
   }
 
-  answer = numbers;
-
-
+  answer = numbers.join("");
   return answer;
 }
-
 console.log(solution([1, 3, 4, 5, 8, 2, 1, 4, 5, 9, 5], "right")); // "LRLLLRLLRRL"
 console.log(solution([7, 0, 8, 2, 8, 3, 1, 5, 7, 6, 2], "left")); //	"LRLLRRLLLRR"
 console.log(solution([1, 2, 3, 4, 5, 6, 7, 8, 9, 0], "right")); // "LLRLLRLLRL"
