f8c92300570de0428df50339bd42234dbb3db686
dreamfulbud <dreamfulbud@naver.com>
2021-11-28 22:58:33 +0900

    코드수정

--- 2week/002.js
+++ 2week/002.js
@@ -2,7 +2,7 @@
 // https://programmers.co.kr/learn/courses/30/lessons/67256
 
 function solution(numbers, hand) {
-  var answer = '';
+  let answer = '';
 
   let leftPos = 10;
   let rightPos = 12;
@@ -14,28 +14,31 @@ function solution(numbers, hand) {
     if (numbers[n] === 1 || numbers[n] === 4 || numbers[n] === 7) {
       leftPos = numbers[n];
       numbers[n] = "L";
-
     } else if (numbers[n] === 3 || numbers[n] === 6 || numbers[n] === 9) {
       rightPos = numbers[n];
       numbers[n] = "R";
-    } else {
+    } else if (numbers[n] === 2 || numbers[n] === 5 || numbers[n] === 8 || numbers[n] === 11) {
       let l = Math.abs(numbers[n] - leftPos);
       let r = Math.abs(numbers[n] - rightPos);
 
       if (l === 1 || l === 3) {
         l = 1;
-      } else if (l === 2 || l === 4) {
+      } else if (l === 2 || l === 4 || l === 6) {
         l = 2;
-      } else if (l === 5 || l === 6 || l === 7) {
+      } else if (l === 5 || l === 7 || l === 9) {
         l = 3;
+      } else if (l === 8 || l === 10) {
+        l = 4;
       }
 
       if (r === 1 || r === 3) {
         r = 1;
-      } else if (r === 2 || r === 4) {
+      } else if (r === 2 || r === 4 || l === 6) {
         r = 2;
-      } else if (r === 5 || r === 6 || r === 7) {
+      } else if (r === 5 || r === 7 || r === 9) {
         r = 3;
+      } else if (r === 8 || r === 10) {
+        r = 4;
       }
 
       if (l === r) {
