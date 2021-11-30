24b89dfff900c484d21d5d5ba47a4fa52ebcc4dc
dreamfulbud <dreamfulbud@naver.com>
2021-12-01 00:12:14 +0900

    3주 3번문제 수정

--- 3week/003.js
+++ 3week/003.js
@@ -4,25 +4,16 @@
 function solution(array, commands) {
   var answer = [];
 
-  let i = 0;
-  let j = 0;
-  let k = 0;
   let arr = [];
-  // i : start ,j : end, k번째 숫자?
-  for (let x = 0; x < 3; x++) {
-    i = commands[x][0] - 1;
-    j = commands[x][1];
-    k = commands[x][2] - 1;
-
-    arr = array.slice(i, j);
-
-    //오름차순 정렬
-    arr.sort((a, b) => a - b);
-    // 내림차순 정렬 : arr.sort((a, b) => b - a);
-    answer.push(arr[k]);
+  for (let i = 0; i < commands.length; i++) {
+    arr = array.slice(commands[i][0] - 1, commands[i][1]);
+    arr.sort((a, b) => a - b); //오름차순 정렬
+    answer.push(arr[commands[i][2] - 1]);
   }
 
   return answer;
 }
 
 console.log(solution([1, 5, 2, 6, 3, 7, 4], [[2, 5, 3], [4, 4, 1], [1, 7, 3]])); //[5,6,3]
+
+    // 내림차순 정렬 : arr.sort((a, b) => b - a);
