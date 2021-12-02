9f7d9301d29b1ee07d4ec5b5fd1b6a4ecd137a7e
dreamfulbud <dreamfulbud@naver.com>
2021-12-02 21:17:33 +0900

    소수 문제 풀이

--- 3week/002.js
+++ 3week/002.js
@@ -2,17 +2,32 @@
 // https://programmers.co.kr/learn/courses/30/lessons/12977
 
 function solution(nums) {
-  var answer = -1;
-  //소수 : 1과 그 수 자신 이외의 자연수로 나눌 수 없는 수
-  // 2,3,5,7,11,13,17,19,23...
+  let answer = 0;
 
-  for (let x of nums) {
-
+  // 3개 뽑기 / 3개의 합 구하기 / 그 합이 소수인지 확인 / 갯수 세기
+  for (let i = 0; i < nums.length - 2; i++) {
+    for (let j = i + 1; j < nums.length - 1; j++) {
+      for (let k = j + 1; k < nums.length; k++) {
+        if (isPrime(nums[i] + nums[j] + nums[k])) {
+          answer++;
+        }
+      }
+    }
   }
 
-  console.log('Hello Javascript')
+  // 소수 구별법
+  // 소수 : 1과 그 수 자신 이외의 자연수로 나눌 수 없는 수
+  // 2,3,5,7,11,13,17,19,23...
+  function isPrime(x) {
+    for (let i = 2; i < x; i++) {
+      if (x % i === 0) {
+        return false;
+      }
+    }
+    return true;
+  }
   return answer;
 }
 
-console.log(solution([1, 2, 3, 4])); //1 Hello Javascript
-console.log(solution([1, 2, 7, 6, 4])); //4 Hello Javascript
+console.log(solution([1, 2, 3, 4])); //1
+console.log(solution([1, 2, 7, 6, 4])); //4
