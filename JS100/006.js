2b09d62a608d0abb69363e67b61a478ede256b59
dreamfulbud <dreamfulbud@naver.com>
2021-12-02 20:47:08 +0900

    4-6문제

---
+++ JS100/006.js
@@ -0,0 +1,18 @@
+/*
+  다음은 자바스크립트 문법 중에서 False로 취급하는 것들 입니다.
+  앗, False로 취급하지 않는 것이 하나 있네요! True를 찾아주세요.
+
+  1)  NaN
+  2)  1
+  3)  ""
+  4)  0
+  5)  undefined
+
+*/
+// 정답은 1 : true
+
+console.log(Boolean(NaN));
+console.log(Boolean(1));
+console.log(Boolean(""));
+console.log(Boolean(0));
+console.log(Boolean(undefined));
