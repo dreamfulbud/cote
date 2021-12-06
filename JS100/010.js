203e48cc34ba1abde27cbf8bed4a8a0752e17a4d
dreamfulbud <dreamfulbud@naver.com>
2021-12-07 08:59:14 +0900

    별찍기 수정

--- JS100/010.js
+++ JS100/010.js
@@ -1,14 +1,45 @@
 const input = 5;
-let star = '';
 
-for (let i = 1; i <= input; i++) {
+//삼각형
+let star = '';
+for (let i = 0; i < input; i++) {
   let line = '';
-  for (let j = input - 1; j >= i; j--) {
-    line += "-";
+  for (let j = input - i - 1; j > 0; j--) {
+    line += " ";
   }
-  for (let k = 1; k <= 2 * i - 1; k++) { //이부분... 공부
-    line += "*";
-  }
-  star += line + `\n`; //이부분 체크
+  line += "*".repeat(2 * i + 1);
+  star += line + `\n`;
 }
 console.log(star);
+
+//역삼각형
+let star2 = '';
+for (let i = 0; i < input; i++) {
+  let line = '';
+  line += " ".repeat(i);
+  for (let j = 2 * (input - i) - 1; j > 0; j--) {
+    line += "*";
+  }
+
+  star2 += line + `\n`; //이부분 체크
+}
+console.log(star2);
+
+
+//별 하나씩 증감
+let star3 = '';
+for (let i = 0; i < input; i++) {
+  let line = '';
+  line += "*".repeat(i + 1);
+  star3 += line + `\n`;
+}
+console.log(star3);
+
+//별 하나씩 빼기
+let star4 = '';
+for (let i = 0; i < input; i++) {
+  let line = '';
+  line += "*".repeat(input - i);
+  star4 += line + `\n`;
+}
+console.log(star4);
