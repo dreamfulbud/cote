213d6ccdb26d90616ec7043c9516b14d5ea30028
dreamfulbud <dreamfulbud@naver.com>
2021-12-11 13:06:07 +0900

    2021.12.11 JS100

---
+++ JS100/023.js
@@ -0,0 +1,9 @@
+/*
+console.log(10/3) 의 출력 결과는 3이다.
+*/
+
+// 정답 X
+
+console.log(10 / 3); //3.3333333333333335
+console.log(Math.floor(10 / 3)); //3
+console.log(Math.round(10 / 3)); //3
---
+++ JS100/024.js
@@ -0,0 +1,11 @@
+function solution(str) {
+  str = str.toUpperCase(); // 대문자 변환
+  // str = str.toLowerCase(); // 소문자 변환
+  return str;
+}
+console.log(solution("mary"));
+console.log(solution("Mary"));
+console.log(solution("MARY"));
+
+
+
