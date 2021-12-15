e92918244a53d9fedd43e223cb56ba965b0ac452
dreamfulbud <dreamfulbud@naver.com>
2021-12-15 19:43:54 +0900

    2021.12.15 JS100제

---
+++ JS100/032.js
@@ -0,0 +1,5 @@
+function solution(str) {
+  return str.split(' ').length;
+}
+console.log(solution("안녕하세요. 저는 제주대학교 컴퓨터공학전공 혜림입니다."));
+
---
+++ JS100/033.js
@@ -0,0 +1,6 @@
+function solution(str) {
+  return str.split(' ').reverse().join(' ');
+}
+console.log(solution("1 2 3 4 5"));
+console.log(solution("2 4 6 7 8"));
+
