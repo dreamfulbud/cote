6e7abc51dcf41f185fa64de3e93ca46564a2f302
강혜진 <dreamfulbud@naver.com>
2022-02-01 16:23:13 +0900

    문제38 재풀이

--- JS100/038.js
+++ JS100/038.js
@@ -1,19 +1,24 @@
 function solution(str) {
-  str = str.split(' ');
-  str.sort((a, b) => a - b);
+  let result = 1;
 
-  let result = 0;
-  let arr = [];
+  let arr = str.split(' ');
+  arr.sort((a, b) => b - a);
 
-  while (arr.length < 3) {
-    let grade = str.pop();
-    if (!arr.includes(grade)) {
-      arr.push(grade);
+  let max = arr[0];
+  let count = 1;
+  for (let i = 1; i < arr.length; i++) {
+    if (max === arr[i]) {
+      result++;
+    } else {
+      max = arr[i];
+      count++;
+      result++;
     }
-    result += 1;
+    if (count === 3) break;
   }
+
   return result;
 }
 console.log(solution("97 86 75 66 55 97 85 97 97 95")); //6
-
+console.log(solution("10 9 8 7 6 5 4")); //6
 
