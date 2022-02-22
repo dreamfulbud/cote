140670805aaceb8daed43a5e75be835dc74f49f1
강혜진 <dreamfulbud@naver.com>
2022-02-22 23:23:38 +0900

    JS100제 58번 콤마찍기 풀이

--- JS100/058.js
+++ JS100/058.js
@@ -1,13 +1,22 @@
 // 콤마찍기
 function solution(num) {
-  num = num + '';
-  if (num.length <= 3) {
-    return num;
-  } else {
-    return solution(num.slice(0, num.length - 3)) + ',' + num.slice(num.length - 3);
-  }
+	let result = "";
+	let arr = String(num).split("").reverse();
+	for (let i = 0; i < arr.length; i++) {
+		if (i !== 0 && i % 3 === 0) {
+			result += `,${arr[i]}`;
+		} else {
+			result += arr[i];
+		}
+	}
+	result = result.split("").reverse().join("");
 
-
+	return result;
 }
-console.log(solution('123456789')); //123,456,789
+
+console.log(solution("123456789")); //123,456,789
 console.log(solution(123456789)); //123,456,789
+console.log(solution("12345678")); //12,345,678
+console.log(solution(12345678)); //12,345,678
+console.log(solution("123")); //12,345,678
+console.log(solution(1234)); //12,345,678
---
+++ JS100/058_.js
@@ -0,0 +1,13 @@
+// 콤마찍기
+function solution(num) {
+	num = num + "";
+	if (num.length <= 3) {
+		return num;
+	} else {
+		return solution(num.slice(0, num.length - 3)) + "," + num.slice(num.length - 3);
+	}
+}
+console.log(solution("123456789")); //123,456,789
+console.log(solution(123456789)); //123,456,789
+console.log(solution("12345678")); //12,345,678
+console.log(solution(12345678)); //12,345,678
